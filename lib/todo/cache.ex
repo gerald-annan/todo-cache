be7af6c4ef7fd18bf19cf819f8b04f38dbbba8df
Gerald Annan <annang2@stanbic.com.gh>
2022-02-17 10:59:50 +0000

    starting todo cache as dynamic server

--- lib/todo/cache.ex
+++ lib/todo/cache.ex
@@ -19,6 +19,21 @@ defmodule Todo.Cache do
     )
   end
 
+  defp start_child(todo_list_name) do
+    DynamicSupervisor.start_child(
+      __MODULE__,
+      {Todo.Server, todo_list_name}
+    )
+  end
+
+  def child_spec(_arg) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, []},
+      type: :supervisor
+    }
+  end
+
   def server_process(process_name) do
     GenServer.call(__MODULE__, {:server_process, process_name})
   end
